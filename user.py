267524d67aab745594720a8de655c48f71338130
Ngichu <brianngichu@gmail.com>
2019-06-17 21:37:26 +0300

    added function to save user

--- user.py
+++ user.py
@@ -10,3 +10,14 @@ class User:
         self.password = password
 
     contact_list = [] # Empty contact list
+    # Init method up here
+
+    def save_user(self):
+
+        '''
+        save_user method saves user objects into user_list
+        '''
+
+        User.user_list.append(self)
+
+    
